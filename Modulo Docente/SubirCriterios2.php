5ceaaf4e2a1ce0cc2abf11cd8509a4b4cb18363a
luisnieto22 <lusnieto5013@gmail.com>
2023-08-03 13:41:56 -0600

    AQUI ESTA LA VERSION ACTUALIZADA

---
+++ Modulo Docente/SubirCriterios2.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Doguito Petshop | Cadastro de cliente</title>
+    <link rel="icon" href="../assets/img/doguitoadm.svg" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../assets/css/base/base.css">
+    <!-- <link rel="stylesheet" href="../assets/css/base/base.css" /> -->
+    <link rel="stylesheet" href="../assets/css/componentes/header.css" />
+    <link rel="stylesheet" href="../assets/css/componentes/card.css" />
+    <link rel="stylesheet" href="../assets/css/register_client.css" />
+    <link rel="stylesheet" href="../assets/css/componentes/inputs.css" />
+    <link rel="stylesheet" href="../assets/css/componentes/button.css" />
+  </head>
+  <?php
+include("nav2.php");
+
+?>
+  <body>
+  <?php
+include_once 'config.inc.php';
+if (isset($_POST['subir'])) {
+    $nombre = $_FILES['archivo']['name'];
+    $tipo = $_FILES['archivo']['type'];
+    $tamanio = $_FILES['archivo']['size'];
+    $ruta = $_FILES['archivo']['tmp_name'];
+    $destino = "archivosCriterios/" . $nombre;
+    if ($nombre != "") {
+        if (copy($ruta, $destino)) {
+            $titulo= $_POST['titulo'];
+            $descri= $_POST['descripcion'];
+            $db=new Conect_MySql();
+            $sql = "INSERT INTO tbl_documentos_Criterios(titulo,descripcion,tamanio,tipo,nombre_archivo) VALUES('$titulo','$descri','$tamanio','$tipo','$nombre')";
+            $query = $db->execute($sql);
+            if($query){
+                ?> 
+                  <h3 class="ok">¡Se ha registrado correctamente!</h3>
+                 <?php
+              } else {
+                  ?> 
+                  <h3 class="bad">¡Ups ha ocurrido un error!</h3>
+                  <?php
+              }
+          } else {
+          ?> 
+          <h3 class="bad">¡Por favor complete los campos!</h3>
+          <?php
+      }     
+  }
+  }
+  ?>
+
+    <main class="container flex flex--center flex--column">
+      <section class="card cadastro">
+        <h1 class="card__title">CRITERIOS PDF</h1>
+
+        <!-- pongo LO DE POST -->
+        <form method="POST" class="flex flex--column" data-form enctype="multipart/form-data">
+   
+     
+          <div class="input-container">
+            <input name="titulo" id="titulo" class="input" type="text" placeholder="Titulo" required data-email/>
+            <label for="exampleFormControlFile1" class="input-label" >Titulo</label>
+            <span class="input-menssage-error">Este campo es requerido</span>
+          </div>
+
+
+          <div class="input-container">
+            <input name="descripcion" id="descripcion" class="input" type="text" placeholder="Descripción" required data-email/>
+            <label for="exampleFormControlFile1"  class="input-label">Descripción</label>
+            <span class="input-menssage-error">Este campo es requerido</span>
+          </div>
+
+          <div class="input-container">
+            <input name="archivo" id="archivo" class="input" type="file" placeholder="archivo" required data-email/>
+            <!-- <label class="input-label" for="archivo">Nombre Cuatrimestre</label>
+            <span class="input-menssage-error">Este campo es requerido</span> -->
+          </div>
+          <button type="submit" name="subir" class="button">Registrar</button>
+          <button type="submit"  class="button"><a href="ConsultalistaCriterios.php" >Consultar</button>
+    
+        </form>
+      </section>
+    </main>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuid.min.js"
+      integrity="sha512-UNM1njAgOFUa74Z0bADwAq8gbTcqZC8Ej4xPSzpnh0l6KMevwvkBvbldF9uR++qKeJ+MOZHRjV1HZjoRvjDfNQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <script type="module" src="../controllers/registro.controller.js"></script>
+  </body>
+</html>
---
+++ Modulo Docente/nav2.php
@@ -0,0 +1,86 @@
+<nav class="navbar navbar-light px-3" style="background-color: #33afce;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="../img/tel.png" alt="" width="25" height="23" class="d-inline-block align-text-top">
+          744-6886416
+          &nbsp;&nbsp;&nbsp;
+          <img src="../img/email.png" alt="" width="25" height="23" class="d-inline-block align-text-top">
+          universidad@utacapulco.edu.mx
+        </a>
+        <span class="navbar-brand">
+          <a href="https://twitter.com/Utacapulco1?fbclid=IwAR0ubBqljQAPuYPeqIghPRinCQqtSGfVIeldjSJVRKl4ukWes3QQB_YPDQc"><img src="../img/tw.png" alt="" width="25" height="23"></a>
+          &nbsp;
+          <a href="https://www.facebook.com/Oficial-Universidad-Tecnol%C3%B3gica-de-Acapulco-1982044402027945"><img src="../img/facebook.png" alt="" width="25" height="23"></a>
+          &nbsp;
+          <a href="https://utacapulco.edu.mx/"><img src="../img/web.png" alt="" width="25" height="23"></a>
+          &nbsp;
+        </span>
+      </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+        <div class="container-fluid">
+            <!-- <img src="../img/logo2.png" width="60" height="60"> -->
+          <a class="navbar-brand" href="indexMa.html">&nbsp; <b> Módulo de Docente</b></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+             
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" 
+                data-bs-toggle="dropdown" aria-expanded="false">
+                  <b>   Secuencia Didactica</b>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                     
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item" href="SubirSecuencia2.php">Subir</a>
+                            <a class="dropdown-item" href="ConsultalistaSecuencia.php">Consultas</a>
+                            </li>
+                    </ul>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" 
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                          <b> Criterios de Evaluación</b>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                             
+                            <li class="dropdown-submenu">
+                                    <a class="dropdown-item" href="SubirCriterios2.php">Subir</a>
+                                    <a class="dropdown-item" href="ConsultalistaCriterios.php">Consultas</a>
+                            </li>
+                        </ul>   
+
+                             <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" 
+                              data-bs-toggle="dropdown" aria-expanded="false">
+                                <b>Actas</b>
+                              </a>
+                              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                           
+                                  <li class="dropdown-submenu">
+                                    <a class="dropdown-item" href="SubirActas2.php">Subir</a>
+                                    <a class="dropdown-item" href="ConsultalistaActas.php">Consultas</a>
+                                       
+                                  </li>
+                              </ul>
+
+                              <li class="nav-item">
+                                
+                                </a>
+                                <a class="nav-link active" aria-current="page" href=""aria-expanded="false">
+                                    <b>Consulta de Evaluacion Docente</b></a>
+                             </li>
+                                        
+
+                           </li>
+                                </li>
+                            </ul>
+                    </li> 
+            </ul>
+          </div>
+        </div>
+      </nav>
